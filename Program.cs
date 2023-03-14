1403b6fe10527281ad1276cf77c5ca8e02d86ac4
Ilya <pulinilija@gmail.com>
2023-03-14 10:53:00 +0700

    task2

--- Program.cs
+++ Program.cs
@@ -1,17 +1,23 @@
-﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
-числа в промежутке от N до 1. Выполнить с помощью рекурсии.
-N = 5 -> "5, 4, 3, 2, 1"
-N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт
+сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
 
-int n;
-Console.Write("Введите число: ");
-int.TryParse(Console.ReadLine()!, out n);
+int m, n;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine(), out m);
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine(), out n);
 
-Console.WriteLine(Print(n, 1));
+Console.WriteLine(Fibb(n));
 
-string Print(int n, int i = 1)
+int Fibb(int item)
 {
-    if (i >= n)
-        return i.ToString();
-    return $"{i}, {Print(n, i + 1)}";
+    if (item < 0)
+        return -1;
+    if (item == m)
+        return m;
+    if (item == 1)
+        return 1;
+    return item + Fibb(item - 1);
 }
